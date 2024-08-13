d7f30cdc8f1acc575a84961ecdac18ccc181cb1b
Brayden Di Luca <bray.diluca@estimateone.com>
2024-08-13 17:11:06 +1000

    Add templ and basic webserver

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/braydend/my-family
 
 go 1.22.6
+
+require github.com/a-h/templ v0.2.747 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/braydend/my-family/components"
+
+	"github.com/a-h/templ"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	component := components.Hello("Bray")
+
+	http.Handle("/", templ.Handler(component))
+
+	fmt.Println("Listening on :3000")
+	http.ListenAndServe(":3000", nil)
 }
