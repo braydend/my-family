8df2e4d92f932c7507b95f40f20d1ed14ef5078a
Brayden <braydendiluca@gmail.com>
2024-08-14 12:13:54 +1000

    Add tests for FamilyTreeService

--- models/familyMember.go
+++ models/familyMember.go
@@ -6,6 +6,10 @@ type FamilyMember struct {
 	Children []FamilyMember
 }
 
+func NewFamilyMember(name string) *FamilyMember {
+	return &FamilyMember{Name: name, Parents: []FamilyMember{}, Children: []FamilyMember{}}
+}
+
 func (f *FamilyMember) AddParent(parent FamilyMember) {
 	f.Parents = append(f.Parents, parent)
 }
